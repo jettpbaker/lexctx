bf72b675a82ff2de71d040a01ca24feadd939cf3
Jett Baker <jett.p.baker@gmail.com>
2026-05-10 15:05:31 +1000

    repair drizzle history

--- drizzle/0022_add_mux_blur_up_placeholder.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "sources" ADD COLUMN "mux_blur_data_url" text;
-ALTER TABLE "sources" ADD COLUMN "mux_blur_aspect_ratio" double precision;
---
+++ drizzle/0022_small_iceman.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "sources" ADD COLUMN "mux_blur_data_url" text;--> statement-breakpoint
+ALTER TABLE "sources" ADD COLUMN "mux_blur_aspect_ratio" double precision;
